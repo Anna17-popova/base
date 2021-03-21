69b4e9e815945aa1377f3f766df88f635f07a7e2
Anna17-popova <74873856+Anna17-popova@users.noreply.github.com>
2021-03-21 20:26:39 +0300

    With bd

--- Mars_mission/mars_main.py
+++ Mars_mission/mars_main.py
@@ -2,25 +2,39 @@ from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('id астронавта', validators=[DataRequired()])
-    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    username2 = StringField('id капитана', validators=[DataRequired()])
-    password2 = PasswordField('Пароль капитана', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
+from data.login import LoginForm
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_restful import abort
 
 
 app = Flask(__name__)
+db_session.global_init("db/mars_explorer.db")
 app.config['SECRET_KEY'] = 'my_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    text = 'Заготовка'
-    return render_template('base.html', title=text)
+    mylist = []
+    db_sess = db_session.create_session()
+    text = 'Журнал работ'
+    for job in db_sess.query(Jobs).all():
+        a = [job.id, job.job, db_sess.query(User).filter(User.id == job.team_leader).first(), job.work_size, job.collaborators, 'Is finished'
+        if job.is_finished == 1 else 'Is not finished']
+        mylist.append(a)
+    print(mylist)
+    return render_template('table_jobs.html', title=text, mylist=mylist)
 
 
 @app.route('/training/<prof>')
@@ -50,8 +64,16 @@ def auto_answer():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/success')
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.name == form.name.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('log.html',
+                                message="Неправильное имя или пароль",
+                                form=form)
     return render_template('log.html', title='Авторизация', form=form)
+
 
 @app.route('/distribution')
 def distribution():
